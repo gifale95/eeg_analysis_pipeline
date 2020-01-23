a4d17f6f0ec9883e85430d8f1022e2b2f999b0ca
ale <alessandro.gifford@gmail.com>
2020-01-23 12:19:17 +0100

    New update 23.01.20 - 12:21

--- eeg_mvnn.py
+++ eeg_mvnn.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # =============================================================================
-# Multivariate noise normalization covariance matrices
+# Multivariate noise normalization covariance matrix
 # =============================================================================
 def main():
-	"""MVNN covriance matrices.
+	"""MVNN covriance matrix.
 
 	Parameters
 	----------
-	sub : int
-	      Subject number (default is 1).
+	prepr_eeg_dir : str
+		  Preprocessed EEG data directory (default is "").
 	mvnn_dim : str
 	      Whether to compute the mvnn covariace matrices
 		  for each time point["time"] or for each epoch["epochs"]
 		  (default is "epochs").
 	freq : int
-	      Downsampling frequency (default is 200).
-	run_where : str (optional)
-	       To run the computations on "local" or "hpc" (default is "local").
+	      Downsampling frequency (default is 1000).
+	out_dir : str
+		  MVNN covariance matrix output directory (default is "").
 
 	Returns
 	-------
@@ -36,13 +36,14 @@ def main():
 ### Input arguments ###
 	parser = argparse.ArgumentParser()
 
-	parser.add_argument('--sub', default=1, type=int, help="subject_n")
+	parser.add_argument("--prepr_eeg_dir", default="",type=str, \
+					help="preprocessed eeg data directory")
 	parser.add_argument('--mvnn_dim', default="time", type=str, \
-					help="mvnn_dimension")
-	parser.add_argument('--freq', default=200, type=int, \
+					help="MVNN dimension")
+	parser.add_argument('--freq', default=1000, type=int, \
 					help="downsampling frequency")
-	parser.add_argument("--run_where", type=str, default="local", \
-						help="To run on 'hpc' or on 'local'")
+	parser.add_argument("--out_dir", default="",type=str, \
+					help="MVNN matrix output directory")
 
 	args = parser.parse_args()
 
@@ -51,82 +52,46 @@ def main():
 	freq = args.freq
 	run_where = args.run_where
 
-	print("\n\n\n>>> Mvnn, %dhz, sub %s <<<" % (freq, sub))
-
-
-### EEG data image conditions directories ###
-	# Working directory
-	if run_where == "local": # if running on local
-
-		workDir = open("/home/ale/workDir_py.txt")
-		workDir = workDir.read()
-		workDir = workDir[:-1]
-
-	else: # if running on hpc
-		
-		workDir = "/scratch/alegifford95/studies/"
-	
-	# Study directory
-	studyDir = "buzz_study/collected_data/"
-
-	# Subject directory
-	subDir = "s" + str(sub) + "/eeg/preprocessed/"
-	
-	# File name
-	fileDir = "preprocessed_" + str(freq) + "hz_sub_" + format(sub, "03") \
-					+ ".npy"
+	print("\n\n\n>>> Multivariate Noise Normalization Matrix <<<")
 
 
 ### Loading the data ###
-	eeg_data = np.load(os.path.join(workDir, studyDir, subDir, fileDir), \
-						allow_pickle=True).item()
-
+	eeg_data = np.load(args.prepr_eeg_dir, allow_pickle=True).item()
+	
+	data = eeg_data["eeg_data"]
+	events = eeg_data["events"]
 	time = eeg_data["time"]
 	channels = eeg_data["channels"]
-	img_category = eeg_data["img_category"]
-	img_fragment = eeg_data["img_fragment"]
-	img_filtering = eeg_data["img_filtering"]
-	eeg_data = eeg_data["eeg_data"]
+	
+	del eeg_data
 
 
 ### Computing the covariance matrices ###
-	n_conditions = len(np.unique(img_category)) * len(np.unique(img_fragment)) * len(np.unique(img_filtering))
+	n_conditions = len(np.unique(events[:,2]))
 
 	# n_conditions * n_sensors * n_sensors
 	sigma_ = np.empty((n_conditions, len(channels), len(channels)))
-	
-	count = 0
 
-	for c in range(len(np.unique(img_category))): # for image category
+	for c in range(len(np.unique(events[:,2]))): # for experimental conditions
 
-		for fr in range(len(np.unique(img_fragment))): # for image fragment
+		# Selecting the condition data
+		idx = events[:,2] == c+1
+		cond_data = data[idx,:,:]
 
-			for fi in range(len(np.unique(img_filtering))): # for image filtering
+		# Performing the computation
+		if mvnn_dim == "time": # if computing covariace matrices for each time point
 
-				# Selecting the data
-				idx_cat = img_category == c+1
-				idx_fra = img_fragment == fr+1
-				idx_fil = img_filtering == fi+1
-				idx = idx_cat & idx_fra & idx_fil
-				
-				cond_data = eeg_data[idx,:,:]
+			# Computing sigma for each time point, then averaging across time
+			sigma_[c] = np.mean([_cov(cond_data[:,:,t], \
+					shrinkage='auto') for t in range(len(time))], \
+					axis=0)
 
-				# Performing the computation
-				if mvnn_dim == "time": # if computing covariace matrices for each time point
+		elif mvnn_dim == "epochs": # if computing covariace matrices for each time epoch
 
-					# Computing sigma for each time point, then averaging across time
-					sigma_[count] = np.mean([_cov(cond_data[:,:,t], \
-							shrinkage='auto') for t in range(len(time))], \
-							axis=0)
-					count += 1
-
-				elif mvnn_dim == "epochs": # if computing covariace matrices for each time epoch
-
-					# Computing sigma for each epoch, then averaging across epochs
-					sigma_[count,:,:] = np.mean([_cov(np.transpose(cond_data[e,:,:]), \
-							shrinkage='auto') for e in range(cond_data.shape[0])], \
-							axis=0)
-					count += 1
+			# Computing sigma for each epoch, then averaging across epochs
+			sigma_[c,:,:] = np.mean([_cov(np.transpose(cond_data[e,:,:]), \
+					shrinkage='auto') for e in range(cond_data.shape[0])], \
+					axis=0)
 
 
 ### Averaging sigma across conditions ###
@@ -146,15 +111,7 @@ def main():
 
 
 ### Saving the covariance matrices ###
-	saveDir = "buzz_study/results/"
-	subDir = "s" + str(sub) + "/mvnn/" + str(freq) + "hz/"
-	fileDir = "mvnn_cov_mat_" + str(freq) + "hz_sub_" + format(sub, "03")
-
-	# Creating the directory if not existing
-	if os.path.isdir(os.path.join(workDir, saveDir, subDir)) == False: # if not a directory
-		os.makedirs(os.path.join(workDir, saveDir, subDir))
-
-	np.save(os.path.join(workDir, saveDir, subDir, fileDir), inv_sigma_dict)
+	np.save(args.out_dir, inv_sigma_dict)
 
 
 ### ###
--- eeg_preprocessing.py
+++ eeg_preprocessing.py
@@ -4,16 +4,20 @@
 # Preprocessing EEG data
 # =============================================================================
 def preprEeg():
-	"""Preprocessing of EEG_encoding_paradigm_2 data.
+	"""Preprocessing of EEG data.
 
 	Parameters
 	----------
-	sub : int
-	      Subject number.
+	eeg_data_dir : str
+		  Raw EEG data directory (default is "").
+	epoch_tmin : float
+		  Epochs min time (default is -0.2).
+	epoch_tmax : float
+		  Epochs max time (default is 0.8).
 	freq : int
 		  Downsampling frequency (default is 1000).
-	run_where : str (optional)
-	       To run the computations on "local" or "hpc" (default is "local").
+	out_dir : str
+		  Preprocessed EEG output directory (default is "").
 
 	Returns
 	-------
@@ -31,94 +35,24 @@ def preprEeg():
 ### Input arguments ###
 	parser = argparse.ArgumentParser()
 
-	parser.add_argument("--sub", default=1, type=int, help="subject_n")
+	parser.add_argument("--eeg_data_dir", default="",type=str, \
+					help="eeg data directory")
+	parser.add_argument("--epoch_tmin", default=-0.2,type=float, \
+					help="epochs min time")
+	parser.add_argument("--epoch_tmax", default=0.6,type=float, \
+					help="epochs max time")
 	parser.add_argument("--freq", default=1000,type=int, \
 					help="downsampling frequency")
-	parser.add_argument("--run_where", type=str, default="local", \
-						help="To run on 'hpc' or on 'local'")
+	parser.add_argument("--out_dir", default="",type=str, \
+					help="preprocessed eeg output directory")
 
 	args = parser.parse_args()
 
-	sub = args.sub
-	freq = args.freq
-	run_where = args.run_where
-
-
-### Data directories ###
-	# Working directory
-	if run_where == "local": # if running on local
-
-		workDir = open("/home/ale/workDir_py.txt")
-		workDir = workDir.read()
-		workDir = workDir[:-1]
-
-	else: # if running on hpc
-		
-		workDir = "/scratch/alegifford95/studies/"
-
-	# Study directory
-	studyDir = "buzz_study/collected_data/"
-
-	# Behavioral data directory
-	behavDir = "s" + str(sub) + "/behavioral/" + format(sub, "03") + ".mat"
-
-	# EEG data directory
-	eegDir = "s" + str(sub) + "/eeg/raw/Buzz_s" + format(sub, "04") + ".vhdr"
-
-
-### Extracting beahavioral data ###
-	# Loading the behavioral data
-	behav = io.loadmat(os.path.join(workDir, studyDir, behavDir))
-	behav = behav["data"]
-
-	# Beahvioral image info
-	img_cat = behav[0][0][1]["scene"][0]
-	img_cat = np.asarray(img_cat, dtype=int)
-
-	img_frag = behav[0][0][1]["fragment"][0]
-	img_frag = np.asarray(img_frag, dtype=int)
-
-	img_filt = behav[0][0][1]["filter"][0]
-	img_filt = np.asarray(img_filt, dtype=int)
-
-
-### Checking the accuracy ###
-	# Extracting the data
-	response = behav[0][0][1]["response"][0]
-	response = np.asarray(response, dtype=int)
-	correctness = behav[0][0][1]["correctness"][0]
-	correctness = np.asarray(correctness, dtype=int)
-
-	# Calculating the accuracy
-	accuracy = sum(correctness) / len(correctness) * 100
-
-	# Establishing the type of errors
-	idx_incorrect = np.where(correctness == 0)
-	false_positives = sum(response[idx_incorrect] == 1)
-	false_negatives = sum(response[idx_incorrect] == 0)
-	no_response = sum(response[idx_incorrect] == 2)
-
-	# Printing the behavioral results
-	print("\n\n\n>>> Preprocessing data %dhz, sub %s <<<" % (freq, sub))
-	print("\nAccuracy: %f %%" % (accuracy))
-	print("False positives: %d" % (false_positives))
-	print("False negatives: %d" % (false_negatives))
-	print("No response: %d\n\n\n" % (no_response))
+	print("\n\n\n>>> Preprocessing data <<<")
 
 
 ### Reading the data ###
-	raw = mne.io.read_raw_brainvision(os.path.join(workDir, studyDir, \
-						eegDir), preload=True)
-
-
-### Selecting only posterior channels ###
-	sel_chan = "^O *|^P *"
-
-	chan_idx = mne.pick_channels_regexp(raw.info["ch_names"], sel_chan)
-	chan_idx = np.asarray(chan_idx)
-	sel_chans = [raw.info["ch_names"][c] for c in chan_idx]
-
-	raw.pick_channels(sel_chans)
+	raw = mne.io.read_raw_brainvision(args.eeg_data_dir, preload=True)
 
 
 ### Finding the data events ###
@@ -127,51 +61,33 @@ def preprEeg():
 
 
 ### Epoching the sequences & baseline correcting ###
-	epochs = mne.Epochs(raw, events, tmin=-.2, tmax=.8, \
+	epochs = mne.Epochs(raw, events, tmin=args.epoch_tmin, tmax=args.epoch_tmax, \
 								baseline=(None,0), preload=True)
 
 	del raw
 
 
-### Dowsampling the sequence data ###
-	epochs.resample(freq)
-
-
-### Removing the target trials ###
-	targets = np.where(img_cat != 0)[0]
-
-	epochs_data = epochs.get_data(); epochs_data = epochs_data[targets,:,:]
-	img_cat = img_cat[targets]
-	img_frag = img_frag[targets]
-	img_filt = img_filt[targets]
+### Downsampling the sequence data ###
+	epochs.resample(args.freq)
 
 
 ### Putting the sequence data into a dictionary ###
 	eeg_data = {
-			"eeg_data": epochs_data,
-			"img_category": img_cat,
-			"img_fragment": img_frag,
-			"img_filtering": img_filt,
+			"eeg_data": epochs.get_data(),
+			"events": events,
 			"channels": epochs.ch_names,
 			"time": epochs.times
 	}
 
-	del epochs, epochs_data
+	del epochs
 
 
 ### Saving the sequence data ###
-	# Saving directories
-	saveDir = "s" + str(sub) + "/eeg/preprocessed/"
-	fileDir = "preprocessed_" + str(freq) + "hz_sub_" + format(sub, "03")
-
-	# Creating the directory if not existing
-	if os.path.isdir(os.path.join(workDir, studyDir, saveDir)) == False: # if not a directory
-		os.makedirs(os.path.join(workDir, studyDir, saveDir))
-
-	np.save(os.path.join(workDir, studyDir, saveDir, fileDir), eeg_data)
+	np.save(out_dir, eeg_data)
 
 
 ### ###
 if __name__ == "__main__":
 	preprEeg()
+
 
